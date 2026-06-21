b8609d9556f81d85ebaf64a23bfe86cebe22d138
anxi <ixs1n01@gmail.com>
2026-06-21 19:43:56 +0300

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -1,25 +1,25 @@
 const customer = {
-    username: "Mango",
-    balance: 24000,
-    discount: 0.1,
-    orders: ["Burger", "Pizza", "Salad"],
-    getBalance() {
-        return this.balance;
-    },
-    getDiscount() {
-        return this.discount;
-    },
-    setDiscount(value) {
-        this.discount = value;
-    },
-    getOrders() {
-        return this.orders;
-    },
-    addOrders(cost, order) {
-        this.balance -= cost - cost * this.discount;
-        this.orders.push(order);
-    },
-}
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+};
 
 customer.setDiscount(0.15);
 console.log(customer.getDiscount()); // 0.15
--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,21 @@
 class Storage {
-    #items;
-    constructor(items) {
-        this.#item = items;
-    }
-    getItems() {
-        return this.#items;
-    }
-    addItem(newItem) {
-        this.#items.push(newItem);
-    }
-    removeItem(itemToRemove) {
-        this.#items = this.#items.filter(item => item !== itemToRemove);
-    }
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
